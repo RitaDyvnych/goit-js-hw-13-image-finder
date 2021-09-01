a76f2c46a79d3e16ed5e0af4c30effbd6147ea2c
RitaDyvnych <m.dyvnych@gmail.com>
2021-09-01 17:55:18 +0300

    добавлен import fetch в import.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 import { error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import debounce from 'lodash/debounce';
-// import CountriesApiServise from './fetchCountries';
+import ImagesApiService from './apiService';
 import cardTpl from './templates/card.hbs';
 import * as basicLightbox from 'basiclightbox';
 import "basiclightbox/dist/basicLightbox.min.css";
