d676db7dc996e26ac0d857c79a79ff3af0229234
RitaDyvnych <m.dyvnych@gmail.com>
2021-09-01 21:25:58 +0300

    стилизирована кнопка Load more, добавлено сообщение об ошибке при неправильном запросе

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "basiclightbox/dist/basicLightbox.min.css";
 const refs = {
 
     form: document.querySelector('.search-form'),
-    // searchInput: document.querySelector('.js-input'),
+    searchInput: document.querySelector('.js-input'),
     output: document.querySelector('.js-output'),
     btnLoad: document.querySelector('.btn')
 }
@@ -33,13 +33,26 @@ function onSearch(e) {
     refs.output.innerHTML = "";
 
     fetchImg.query = refs.form.elements.query.value;
-    fetchImg.fetchImages().then(result => { createMarkup(result) });
+    fetchImg.fetchImages().then(result => { createMarkup(result) })
+        .catch(error => { console.log(error) });
+    // refs.searchInput = "";
     
 }
 
 function createMarkup(result) {
+
+    if (result.hits.length === 0) {
+        error(
+            {
+                delay: 2000,
+                text: "Not found",
+                maxOpen: 1,
+            })
+        return
+    }
     refs.output.insertAdjacentHTML('beforeend', cardTpl(result.hits));
     refs.btnLoad.classList.remove('is-hidden');
+    
 }
 
 function onBtnLoadClick() {
