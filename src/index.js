df0cb1dcd4c9fb71d5f4de2c7eaf204129f5c305
RitaDyvnych <m.dyvnych@gmail.com>
2021-09-01 15:39:13 +0300

    установлен parsel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/main.scss';
+// import { error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import debounce from 'lodash/debounce';
+// import CountriesApiServise from './fetchCountries';
+// import countryTpl from './templates/country.hbs';
+// import countriesTpl from './templates/countries.hbs';
+
+
