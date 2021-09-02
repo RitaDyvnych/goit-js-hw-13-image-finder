371d8ae92aa494591314174785720d8c12cb7950
RitaDyvnych <m.dyvnych@gmail.com>
2021-09-02 21:28:41 +0300

    добавлена кнопка back to top

--- src/index.js
+++ src/index.js
@@ -78,3 +78,26 @@ function fetchImage() {
     });
     
 }
+
+// back to top button
+let backToTopBtn = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+backToTopBtn.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
